62a1c0ff7b8fcbe88c51e863de6b7fade31b7a4f
Dom <Domhill1998@gmail.com>
2020-01-06 21:24:33 +0000

    Add project files.

---
+++ ThreeAmigosReveseProxy/Controllers/AdminProxyController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ThreeAmigosReveseProxy.Controllers
+{
+    [Route("Admin/")]
+    [ApiController]
+    public class AdminProxyController : ControllerBase
+    {
+        #region Staff
+        [Route("Staff")]
+        public ActionResult RerouteStaffPage()
+        {
+            string uri = "https://localhost:44317/staff/";
+            return Redirect(uri);
+        }
+        #endregion
+
+
+        #region Orders
+        [Route("Orders/PendingDispatch")]
+        public ActionResult ReroutePendingOrdersPage()
+        {
+            string uri = "https://orders-frontend-prod.azurewebsites.net/Order/PendingDispatch/";
+            return Redirect(uri);
+        }
+
+        [Route("Orders/Invoice")]
+        public ActionResult RerouteInvoceOrdersPage()
+        {
+            string uri = "https://invoices-frontend-prod.azurewebsites.net/invoice/";
+            return Redirect(uri);
+        }
+
+
+        #endregion
+
+
+
+
+
+    }
+}
---
+++ ThreeAmigosReveseProxy/Controllers/GeneralProxyController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ThreeAmigosReveseProxy.Controllers
+{   
+    [ApiController]
+    public class GeneralProxyController : ControllerBase
+    {
+
+        //Have a look at introducing middleware to this.
+
+        #region Products
+        [Route("Products")]
+        public ActionResult RerouteProductsPage()
+        {
+            string uri = "https://msproductsfrontend.azurewebsites.net/";
+            return Redirect(uri);
+        }
+        #endregion
+
+        #region Orders
+        [Route("Orders/Active")]
+        public ActionResult RerouteActiveOrdersPage()
+        {
+            string uri = "https://orders-frontend-prod.azurewebsites.net/Order/Active/";
+            return Redirect(uri);
+        }
+
+        [Route("Orders/History")]
+        public ActionResult RerouteHistoryOrdersPage()
+        {
+            string uri = "https://orders-frontend-prod.azurewebsites.net/Order/History/";
+            return Redirect(uri);
+        }
+
+        
+        #endregion
+
+
+        #region New Product
+        [Route("NewProduct/CreateSearch")]
+        public ActionResult RerouteCreateSearchPage()
+        {
+            string uri = "https://localhost:44353/NewProduct/CreateSearch/";
+            return Redirect(uri);
+        }
+
+        [Route("NewProduct")]
+        public ActionResult RerouteNewProductsPage()
+        {
+            string uri = "https://localhost:44353/NewProduct/";
+            return Redirect(uri);
+        }
+
+        #endregion
+
+
+
+
+
+    }
+}
